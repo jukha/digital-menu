a8d42a3714d52102cd42415fd63cd50376dcdd12
jukha <junaid.apsc@gmail.com>
2023-11-15 12:38:20 +0500

    fix: All the issues mentioned in G-Doc

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 "use strict";
 
+const decrementBtn = document.querySelector(".decrement-btn");
+const incrementBtn = document.querySelector(".increment-btn");
+const itemQuantity = document.querySelector(".item-quantity");
+
+function handleIncrement() {
+  itemQuantity.textContent = Number(itemQuantity.textContent) + 1;
+}
+function handleDecrement() {
+  if (Number(itemQuantity.textContent) < 2) return;
+  itemQuantity.textContent = Number(itemQuantity.textContent) - 1;
+}
+
+incrementBtn.addEventListener("click", handleIncrement);
+decrementBtn.addEventListener("click", handleDecrement);
+
 window.addEventListener("scroll", () => {
   const scrollPosition = window.scrollY || window.pageYOffset;
 
-  if (scrollPosition >= 50) {
+  if (scrollPosition >= 250) {
     document.body.classList.add("scrolled");
   } else {
     document.body.classList.remove("scrolled");
